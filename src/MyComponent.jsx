dbb7aaf804a9ac8a517a7492305e98058e10598a
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-10 18:53:03 +0600

    remove function

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -20,7 +20,7 @@ function MyComponent(){
   }
 
   function handleRemoveCar(index){
-
+    setCars(c => c.filter((_, i) => i !== index))
     
   }
 
@@ -40,7 +40,7 @@ function MyComponent(){
             <h2>List of Car Objects</h2>
             <ul>
               {cars.map((car, index) => 
-              <li key={index}> 
+              <li key={index} onClick={() => handleRemoveCar(index)}> 
                   {car.year} {car.make} {car.model}
               </li>)}
             </ul>

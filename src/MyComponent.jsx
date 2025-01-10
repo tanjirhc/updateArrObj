5dbd2f1fbdd085d8c8de55a7bbdb43e00537fa45
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-10 18:47:10 +0600

    handleAddCar function

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -8,31 +8,49 @@ function MyComponent(){
   const [carModel, setCarModel] = useState("");
 
   function handleAddCar(){
+    const newCar = {  year: carYear, 
+                      make: carMake, 
+                      model: carModel};
 
+    setCars(c => [...c, newCar]);
+
+    setCarYear(new Date().getFullYear());
+    setCarMake("");
+    setCarModel("");
   }
 
   function handleRemoveCar(index){
 
+    
   }
 
   function handleYearChange(event){
-
+    setCarYear(event.target.value);
   }
 
   function handleMakeChange(event){
-
+    setCarMake(event.target.value);
   }
 
   function handleModelChange(event){
-
+    setCarModel(event.target.value);
   }
 
   return( <div>
             <h2>List of Car Objects</h2>
             <ul>
-
+              {cars.map((car, index) => 
+              <li key={index}> 
+                  {car.year} {car.make} {car.model}
+              </li>)}
             </ul>
-            <input type="text" value={carYear}/>
+            <input type="number" value={carYear} onChange={handleYearChange}/> <br/>
+            <input type="text" value={carMake} onChange={handleMakeChange}
+                placeholder='Enter car make'/><br/>
+            <input type="text" value={carModel} onChange={handleModelChange}
+                placeholder='Enter car model'/><br/>
+            
+            <button onClick={handleAddCar}>Add Car</button>
 
           </div>)
 }
